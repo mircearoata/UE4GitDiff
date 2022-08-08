bd7a2a1eb9ac293ad289734deca0a377ce853573
mircearoata <mircearoatapalade@gmail.com>
2022-08-08 15:09:41 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/mircearoata/UEGitDiff
+
+go 1.18
+
+require (
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/sys v0.0.0-20220804214406-8e32c043e418 // indirect
+)
---
+++ listengines_windows.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/pkg/errors"
+	"golang.org/x/sys/windows/registry"
+)
+
+func listEngines() (map[string]string, error) {
+	k, err := registry.OpenKey(registry.CURRENT_USER, `SOFTWARE\Epic Games\Unreal Engine\Builds`, registry.QUERY_VALUE)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to open registry key")
+	}
+	defer k.Close()
+
+	engines := make(map[string]string)
+
+	values, err := k.ReadValueNames(0)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to read registry values")
+	}
+
+	for _, value := range values {
+		path, _, err := k.GetStringValue(value)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to get registry value")
+		}
+		engines[value] = path
+	}
+
+	return engines, nil
+}
